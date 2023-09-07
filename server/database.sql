fe24417f19771e4b1b4574fdf8689aa1d3db7805
dhanajeane79 <dhanajeane79@gmail.com>
2023-09-07 11:37:51 -0400

    first commit

---
+++ server/database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE CompCareProducts;
+
+CREATE TABLE product(
+    product_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
---
+++ server/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req body
+
+
+
+
+//Routes//
+
+//create a product
+app.post("/products", async (req, res) => {
+    try {
+      const { name } = req.body;
+      const newProduct = await pool.query(
+        "INSERT INTO product (name) VALUES($1) RETURNING *",
+        [name]
+      );
+  
+      res.json(newProduct.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
+//get all products
+app.get("/products", async (req, res) => {
+    try {
+      const allProducts = await pool.query("SELECT * FROM product");
+  
+      res.json(allProducts.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+  
+
+//get a single product
+app.get("/products/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const product = await pool.query("SELECT * FROM product WHERE product_id = $1", [
+        id,
+      ]);
+  
+      res.json(product.rows[0]);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+
+//update a product
+
+//delete a product
+
+
+app.listen(4000, () => {
+    console.log("The server has started on Port 4000");
+  });
+  
