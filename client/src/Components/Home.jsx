7dfc9654ae5b41e0327b8ac9b6600c7fe2ba080c
dhanajeane79 <dhanajeane79@gmail.com>
2023-09-28 10:01:55 -0400

    Changed name and added Welcome Message

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -16,15 +16,9 @@ function Home() {
   return (
     <div className="home-container">
       <header className="home-header">
-        <h1>Welcome to Compassionate Care Emporium</h1>
+        <h1>Welcome to Life's Little Moments</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        Caregiving is a journey of love, compassion, and selflessness. It's about being there when it matters the most, offering a helping hand, and providing unwavering support to the ones we hold dear. As caregivers, we understand the profound impact of our role – it's not just about tending to physical needs, but also nourishing the soul.
         </p>
       </header>
 
